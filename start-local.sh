7235fefce71b5d913edf77afd08340c269e61f88
Fabio <fabiojd47@gmail.com>
2024-08-08 16:08:58 -0300

    Base docker versions adjudments

--- start-local.sh
+++ start-local.sh
@@ -6,8 +6,8 @@ docker-compose up --force-recreate -d
 
 docker exec -it $CONTAINER_BACK_NAME composer install
 docker exec -it $CONTAINER_BACK_NAME php artisan migrate
-docker exec -it $CONTAINER_BACK_NAME service cron stop
-docker exec -it $CONTAINER_BACK_NAME cron
+#docker exec -it $CONTAINER_BACK_NAME service cron stop
+#docker exec -it $CONTAINER_BACK_NAME cron
 ./chown-files.sh $PROJECT_FRONT_DIR
 ./chown-files.sh $PROJECT_BACK_DIR
 docker exec -it $CONTAINER_BACK_NAME chown -R nginx:nginx /var/www/app/storage
@@ -19,7 +19,7 @@ docker exec -it $CONTAINER_BACK_NAME chown -R nginx:nginx /var/www/app/bootstrap
 #docker exec -d -it $CONTAINER_BACK_NAME sh /var/www/app/exec-pipe.sh
 
 docker ps
-docker exec -it $CONTAINER_BACK_NAME service cron status
+#docker exec -it $CONTAINER_BACK_NAME service cron status
 
 echo ""
 echo "Acesse o sistema em: http://localhost:$CONTAINER_FRONT_EXTERNAL_PORT"
